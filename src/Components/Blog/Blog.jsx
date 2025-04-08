053d70b2ac48c3156eea628e6cd2b0e203f61994
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-08 19:41:46 +0600

    5 react icon added and some design also

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa";
 
 const Blog = ({blog}) => {
   console.log(blog)
-  const {cover,title} = blog;
+  const {cover,title,author_img,author} = blog;
   return (
     <div>
       
@@ -13,8 +14,18 @@ const Blog = ({blog}) => {
       alt="Shoes" />
   </figure>
   <div className="card-body">
+    <div className="author flex justify-around items-center">
+      <h3>{author}</h3>
+      <img className='w-16' src={author_img} alt="" />
+      <FaBookmark size={30}/>
+    </div>
     <h2 className="card-title">{title}</h2>
     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="flex">
+      {
+        blog.hashtags.map(has => <p> {has} </p>)
+      }
+    </div>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">mark as read</button>
     </div>
