30e2b3e8f99b6c41541a2b6fa135c6c310910f92
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-08 18:25:50 +0600

    2 fake data , and data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 
 
 import './App.css'
+import Blogs from './Components/Navbar/Blogs/Blogs'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      
+      <Navbar></Navbar>
+      <Blogs></Blogs>
     </>
   )
 }
 
 export default App
+      
+      
